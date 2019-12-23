704e41de9d8ac21ad8bed53cc1b3a9ccb21a0a5e
Panov Yuri <35775430+PanovYuri@users.noreply.github.com>
2019-12-23 16:58:27 +0500

    add build version

---
+++ build/precache-manifest.9f583114ddc533537f95029c8df2a028.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7af8e1ff2aaed962078a5ecc6f2ce139",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76736150842d261fd98f",
+    "url": "/static/css/main.859c9cdd.chunk.css"
+  },
+  {
+    "revision": "06f44d7c6eeee966ea35",
+    "url": "/static/js/2.34ad89f7.chunk.js"
+  },
+  {
+    "revision": "76736150842d261fd98f",
+    "url": "/static/js/main.3ae71bb9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
